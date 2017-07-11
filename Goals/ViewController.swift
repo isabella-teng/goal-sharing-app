5faeeff8db1db9813179d337c84d994b9cfd469d
Isabella Teng <isabellateng@fb.com>
2017-07-11 10:50:28 -0700

    Initial Commit

---
+++ Goals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goals
+//
+//  Created by Isabella Teng on 7/11/17.
+//  Copyright © 2017 Isabella Teng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
