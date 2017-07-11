312f8c74426e0372836e235da2815c7c2f87a840
Gerardo Parra <gparra@fb.com>
2017-07-11 14:18:00 -0700

    replaced Post.swift with Update.swift

--- Goals/Post.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Post.swift
-//  Goals
-//
-//  Created by Gerardo Parra on 7/11/17.
-//  Copyright © 2017 Isabella Teng. All rights reserved.
-//
-
-import Foundation
---
+++ Goals/Update.swift
@@ -0,0 +1,25 @@
+//
+//  Post.swift
+//  Goals
+//
+//  Created by Gerardo Parra on 7/11/17.
+//  Copyright © 2017 Isabella Teng. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Update: PFObject {
+    // Properties
+//    var id: Int
+//    var title: String
+//    var description: String
+//    var likes: [User]
+//    var likeCount: Int
+//    var comments: [Comment]
+//    var categories: [String]
+//    
+//    init(dictionary: [String: Any]) {
+//        
+//    }
+}
