103e253513fea3cfdd2736cbb90d46f6aea91dc6
Isabella Teng <isabellateng@fb.com>
2017-07-11 14:13:24 -0700

    Clean up pods

---
+++ Goals/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  Goals
+//
+//  Created by Isabella Teng on 7/11/17.
+//  Copyright © 2017 Isabella Teng. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class User: NSObject {
+    //Properties of user
+    
+    var name: String
+    var screenName: String
+    var profPicURL: URL
+    //Add more properties later
+    
+    //User persistance
+//    var dictionary: [String: Any]?
+//    private static var _current: User?
+//    
+//    //create User
+//    let user = PFObject(className: "User")
+//    
+//    //Initialize the user object
+//    init() {
+//        user["username"] =
+//    }
+    
+    
+    
+    
+}
