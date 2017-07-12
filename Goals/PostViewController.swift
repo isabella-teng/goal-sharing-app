e0d36c28fea482d429537749706297af118df7e1
Gerardo Parra <gparra@fb.com>
2017-07-12 15:32:18 -0700

    customized post goal view

--- Goals/PostViewController.swift
+++ Goals/PostViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var postButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
@@ -17,20 +17,17 @@ class PostViewController: UIViewController {
     @IBOutlet weak var typeControl: UISegmentedControl!
     @IBOutlet weak var categoryControl: UISegmentedControl!
     
-    @IBAction func cancelPost(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+    var textHasBeenEdited = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         postButton.layer.cornerRadius = 10
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        descriptionTextView.delegate = self
+        descriptionTextView.text = "Describe your goal briefly"
+        descriptionTextView.textColor = UIColor.lightGray
+        titleTextField.becomeFirstResponder()
     }
    
     // Temporary
@@ -47,6 +44,45 @@ class PostViewController: UIViewController {
         data["categories"] = goalCategory
         
         Goal.createGoal(data: data)
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // Placeholder, disabled button functionality
+    func textViewDidBeginEditing(_ textView: UITextView) {
+       descriptionTextView.text = ""
+        if !textHasBeenEdited {
+            postButton.isEnabled = false
+            postButton.alpha = 0.7
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if descriptionTextView.text.isEmpty {
+            textHasBeenEdited = false
+            descriptionTextView.text = "Describe your goal briefly"
+            descriptionTextView.textColor = UIColor.lightGray
+            postButton.isEnabled = false
+            postButton.alpha = 0.7
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if !textHasBeenEdited {
+            descriptionTextView.text = String(descriptionTextView.text.characters.prefix(1))
+            descriptionTextView.textColor = UIColor.black
+            textHasBeenEdited = true
+        } else {
+            postButton.isEnabled = true
+            postButton.alpha = 1.0
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
     /*
