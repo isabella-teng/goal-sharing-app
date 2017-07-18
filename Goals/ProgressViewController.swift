96a437b0bc38426b9e95c0160d41c2c8d8c44a0f
Josh Olumese <joshlume@fb.com>
2017-07-18 16:39:14 -0700

    Merge Conflicts

--- Goals/ProgressViewController.swift
+++ Goals/ProgressViewController.swift
@@ -2,62 +2,19 @@
 //  ProgressViewController.swift
 //  Goals
 //
-//  Created by Josh Olumese on 7/17/17.
+//  Created by Josh Olumese on 7/18/17.
 //  Copyright © 2017 Isabella Teng. All rights reserved.
 //
 
 import UIKit
-import Charts
 
-
-class ProgressViewController: UIViewController, ChartViewDelegate {
-
-    @IBOutlet weak var barChartView: BarChartView!
-    @IBAction func cancelButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    var months: [String] = []
+class ProgressViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
-        barChartView.delegate = self
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
-        
-        setChart(dataPoints: months, values: unitsSold)
-        
-        
     }
-    
-    func setChart(dataPoints: [String], values: [Double]) {
-        barChartView.noDataText = "You need to provide data for the chart."
-        barChartView.chartDescription?.text = ""
-        
-        var dataEntries: [BarChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [values[i]])
-            dataEntries.append(dataEntry)
-        }
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        barChartView.data = chartData
-        
-        //Settings for the Bar graph
-        chartDataSet.colors = ChartColorTemplates.vordiplom()
-        barChartView.xAxis.labelPosition = .bottom
-        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
-        
-        // Target or Goal Line
-        let lineLimit = ChartLimitLine(limit: 10, label: "Target")
-        barChartView.rightAxis.addLimitLine(lineLimit)
-    }
-    
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
