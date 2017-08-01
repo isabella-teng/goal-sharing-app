89495ece09881b2f1794ecc1a4b84804037a769a
Josh Olumese <joshlume@fb.com>
2017-08-01 16:11:15 -0700

    minor tweak

--- Goals/UserViewController.swift
+++ Goals/UserViewController.swift
@@ -30,7 +30,7 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ContainerCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserSearchCell", for: indexPath) as! UserSearchCell
         return cell
     }
 
