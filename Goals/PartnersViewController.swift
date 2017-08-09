1b8897509b1202c9e4474c7dead16d0c5beb281b
Gerardo Parra <gparra@fb.com>
2017-08-09 10:43:16 -0700

    change mark's progress

--- Goals/PartnersViewController.swift
+++ Goals/PartnersViewController.swift
@@ -23,8 +23,8 @@ class PartnersViewController: UIViewController, UITableViewDataSource, UITableVi
         tableView.estimatedRowHeight = 180
         
         partners = [ [ "username": "auggie", "icon": #imageLiteral(resourceName: "auggie"), "goalTitle": "Save money for a new laptop", "streak": 0, "alerts": 0, "trend": "negative", "chartData": [5, 4, 5, 3, 2, 0, 0] ],
-                     [ "username": "zuck", "icon": #imageLiteral(resourceName: "zuck"), "goalTitle": "Connect the world", "streak": 0, "alerts": 1, "trend": "negative", "chartData": [9, 15, 18, 6, 3, 3, 0] ],
                     [ "username": "alina", "icon": #imageLiteral(resourceName: "alina"), "goalTitle": "Meet Zuck", "streak": 1, "alerts": 2, "trend": "neutral", "chartData": [2, 3, 5, 4, 3, 6, 5] ],
+                    [ "username": "zuck", "icon": #imageLiteral(resourceName: "zuck"), "goalTitle": "Connect the world", "streak": 6, "alerts": 1, "trend": "positive", "chartData": [9, 10, 8, 6, 3, 9, 17] ],
                     [ "username": "mei", "icon": #imageLiteral(resourceName: "mei"), "goalTitle": "Get a return offer", "streak": 10, "alerts": 4, "trend": "positive", "chartData": [2, 1, 3, 3, 5, 4, 7] ] ]
     }
     
